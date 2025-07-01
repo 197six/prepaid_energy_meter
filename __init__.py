902b26a66cefbc325fbb6f30ac6eaa2d88710da7
197six <github@fishley.co.za>
2025-07-01 18:44:57 +0200

    fix init

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@ from .const import DOMAIN
 async def async_setup_entry(hass, config_entry):
     """Set up Prepaid Energy Meter from a config entry."""
     hass.data.setdefault(DOMAIN, {})
-    await hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    await hass.async_forward_entry_setup(config_entry, "sensor")
     return True
 
 async def async_unload_entry(hass, config_entry):
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    return await hass.async_forward_entry_unload(config_entry, "sensor")
